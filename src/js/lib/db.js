fc59f526290e3e5ee62cf4e140322515ebbcf462
Brian Link <cpsubrian@gmail.com>
2016-01-04 02:12:39 -0800

    Adds a body background.

--- src/js/lib/db.js
+++ src/js/lib/db.js
@@ -1,40 +1,38 @@
 import Firebase from 'firebase'
 
 const db = new Firebase('https://hikertales.firebaseio.com')
-const api = {}
 
-/* Auth
- ******************************************************************************/
+class API {
 
-// Fetch current auth state.
-api.fetchAuth = function (cb) {
-  try {
-    cb(null, db.getAuth())
-  } catch (err) {
-    cb(err)
+  /* Auth
+   ****************************************************************************/
+  fetchAuth (cb) {
+    try {
+      cb(null, db.getAuth())
+    } catch (err) {
+      cb(err)
+    }
+  }
+
+  login (provider, cb) {
+    if (provider === 'twitter') {
+      db.authWithOAuthPopup('twitter', (err, authData) => {
+        if (err) return cb(err)
+        cb(null, authData)
+      })
+    } else {
+      cb(new Error('Not a valid authentication provider'))
+    }
+  }
+
+  logout (cb) {
+    try {
+      db.unauth()
+      cb()
+    } catch (err) {
+      cb(err)
+    }
   }
 }
 
-// Login
-api.login = function (provider, cb) {
-  if (provider === 'twitter') {
-    db.authWithOAuthPopup('twitter', function (err, authData) {
-      if (err) return cb(err)
-      cb(null, authData)
-    })
-  } else {
-    cb(new Error('Not a valid authentication provider'))
-  }
-}
-
-// Logout
-api.logout = function (cb) {
-  try {
-    db.unauth()
-    cb()
-  } catch (err) {
-    cb(err)
-  }
-}
-
-export default api
+export default new API()
