b7d04a34b679c3e70735fa2e8761b443db52faa1
Brian Link <cpsubrian@gmail.com>
2016-01-22 18:27:04 -0800

    More stuff.

--- src/js/components/routes/Home.js
+++ src/js/components/routes/Home.js
@@ -8,8 +8,29 @@ class HomeController extends React.Component {
 
   render () {
     return (
-      <div className='home'>
-        Home
+      <div className='route--home'>
+        <div className='pitch'>
+          <h2>Share Your Journey.</h2>
+          <h2>Tell Your Tale.</h2>
+        </div>
+        <div className='browse'>
+          <div className='updates'>
+            <h3>Recent Updates</h3>
+            <ul>
+              <li>Update</li>
+              <li>Update</li>
+              <li>Update</li>
+            </ul>
+          </div>
+          <div className='hikes'>
+            <h3>Latest Hikes</h3>
+            <ul>
+              <li>Hike</li>
+              <li>Hike</li>
+              <li>Hike</li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
